1ada92b6b4f86c0afde23b8ddec8f3df1a542b9f
Joanna Carrión Pérez <77378686+Joanna20Carrion@users.noreply.github.com>
2025-04-14 00:34:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def upload():
     return send_file(img, mimetype='image/png', as_attachment=True, download_name='grafico_despacho_diesel.png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
